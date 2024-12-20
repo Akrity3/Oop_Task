3528c3f9336f443c6ce9badca7e0b9c3813640e2
Akrity3 <163756385+Akrity3@users.noreply.github.com>
2024-12-20 20:05:02 +0545

    Modified

--- Hello.java
+++ Hello.java
@@ -1,7 +1,12 @@
 public class Hello{
     public static void main(String[] args){
-        // 1. Write a program that prints Hello World! to the console. Save the program as Hello.java.
-        System.out.println("Hello World!");
-
+        // 2. Extend/Modify the above program to print two additional lines:
+        // This is my first program
+        // I am on module ST4003CEM
+        // System.out.print (not println) and produces the same output.
+        System.out.print("Hello World!\n");
+        System.out.print("This is my frist program.\n");
+        System.out.print("I am on module ST4003CEM.\n");
+      
     }
 }
